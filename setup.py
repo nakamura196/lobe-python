5e2e998768c2d2805f49bfde6b2ca3bbfbc01a8e
Satoru Nakamura <na.kamura.1263@gmail.com>
2020-12-27 08:38:52 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
         "numpy",
         "pillow",
         "requests",
-        "tensorflow>=1.14.0,<2;platform_machine!='armv7l'",
+        "tensorflow>=1.14.0",
         "tflite_runtime ; platform_machine=='armv7l'"
     ],
     dependency_links=[
